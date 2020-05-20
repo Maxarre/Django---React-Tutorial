9356dad0ed29a333e74067e28745a3b276f2c070
Maxence Poncelet <maxenceponcelet@hotmail.com>
2020-05-20 22:29:50 +0200

    Layout

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -7,7 +7,9 @@ import CustomLayout from './containers/Layout';
 function App() {
   return (
     <div className="App">
-      <CustomLayout />
+      <CustomLayout>
+
+      </CustomLayout>
     </div>
   );
 }

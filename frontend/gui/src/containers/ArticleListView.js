5757b5e6e62be1b3d181675ad6a725e489382f25
Maxence Poncelet <maxenceponcelet@hotmail.com>
2020-05-21 00:19:01 +0200

    ArticleDetails Added

--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -3,25 +3,20 @@ import axios from "axios";
 import Articles from "../components/Article";
 // import CustomForm from "../components/Form";
 
-const listData = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://ant.design',
-    title: `ant design part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
-
 class ArticleList extends React.Component {
   state = {
     articles: []
   };
 
-  fetchArticles = () => {
+//   fetchArticles = () => {
+//     axios.get("http://127.0.0.1:8000/api/").then(res => {
+//       this.setState({
+//         articles: res.data
+//       });
+//     });
+//   }
+
+  componentDidMount() {
     axios.get("http://127.0.0.1:8000/api/").then(res => {
       this.setState({
         articles: res.data
@@ -29,20 +24,16 @@ class ArticleList extends React.Component {
     });
   }
 
-  componentDidMount() {
-    this.fetchArticles();
-  }
-
-  componentWillReceiveProps(newProps) {
-    if (newProps.token) {
-      this.fetchArticles();      
-    }
-  }
+//   componentWillReceiveProps(newProps) {
+//     if (newProps.token) {
+//       this.fetchArticles();      
+//     }
+//   }
 
   render() {
     return (
       <div>
-        <Articles data={listData} /> <br />
+        <Articles data={this.state.articles} /> <br />
         <h2> Create an article </h2>
         {/* <CustomForm requestType="post" articleID={null} btnText="Create" /> */}
       </div>
